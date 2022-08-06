945663fc00f3939f08b8582c4f43fbc653b1c4a9
kodapassey <kodapassey@charter.net>
2022-08-06 09:25:29 -0700

    comment routes complete

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,19 +3,44 @@ const { Comment } = require('../../models');
 
 
 router.get('/', (req, res) => {
-    Comment.findAll({
-
-    })
+    Comment.findAll()
+        .then(commentData => res.json(commentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
 });
 
 router.post('/', (req, res) => {
     Comment.create({
-
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
     })
+        .then(commentData => res.json(commentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
 });
 
 router.delete('/', (req, res) => {
     Comment.destroy({
-        
+        where: {
+            id: req.params.id
+        }
     })
+        .then(commentData => {
+            if (!commentData) {
+                res.status(400).json({ message: 'No comment found with that id' });
+                return;
+            }
+            res.json(commentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
+
+module.exports = router;
